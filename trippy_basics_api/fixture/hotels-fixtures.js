c8004fa560e15cba73fd7b4fe76ff5048194b770
Amir <amirhatam136@gmail.com>
2022-01-29 22:06:56 +0100

    add hotel fixtures

---
+++ trippy_basics_api/fixture/hotels-fixtures.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const hotelModel = require('../models/hotelsModel')
+
+mongoose.connect('mongodb://localhost:27017/trippy', (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Server was connected to the Database");
+    }
+})
+
+const addHotels = async () => {
+    try {
+        await hotelModel.deleteMany({})
+        await hotelModel.insertMany([
+            {
+                name: "HBC",
+                address: "12 rue Passy,16éme",
+                city: "Paris",
+                country: "France",
+                stars: 6,
+                hasSpa: true,
+                hasPool: true,
+                priceCategory: 200,
+            },
+            {
+                name: "Tivoli",
+                address: "12 street M.J",
+                city: "New-York",
+                country: "USA",
+                stars: 6,
+                hasSpa: true,
+                hasPool: true,
+                priceCategory: 250,
+            }
+        ])
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+addHotels()
