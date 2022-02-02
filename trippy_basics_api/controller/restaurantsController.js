a751fce86798a51e3c5d9f734333ccff668f36ed
Amir <amirhatam136@gmail.com>
2022-02-02 20:33:51 +0100

    add the functionality PUT,update for restaurant

--- trippy_basics_api/controller/restaurantsController.js
+++ trippy_basics_api/controller/restaurantsController.js
@@ -43,4 +43,26 @@ const getRestaurant = async (req, res) => {
     }
 }
 
-module.exports = { getRestaurantes, addRestaurants, getRestaurant }
+const updateRestaurant = async (req, res) => {
+    try {
+        const restaurantId = req.params.id
+
+        //update with query
+        const queryData = req.query.name
+        const updateRestaurant = await restaurantsModel.updateOne({ _id: restaurantId }, { name: queryData })
+
+        //update with body
+        // const newData = req.body
+        // const updateRestaurant = await restaurantsModel.updateOne({ _id: hotel }, newData)
+
+        res.json({
+            message: "Restaurant was updated",
+            updateRestaurant
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ errorMessage: "There was a problem :(" })
+    }
+}
+
+module.exports = { getRestaurantes, addRestaurants, getRestaurant, updateRestaurant }
--- trippy_basics_api/routes/restaurantsRoute.js
+++ trippy_basics_api/routes/restaurantsRoute.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { getRestaurantes, addRestaurants, getRestaurant } = require("../controller/restaurantsController")
+const { getRestaurantes, addRestaurants, getRestaurant, updateRestaurant } = require("../controller/restaurantsController")
 
 router.get('/', getRestaurantes)
 router.post('/', addRestaurants)
 router.get('/:id', getRestaurant)
+router.put('/:id', updateRestaurant)
 
 router.all("*", (req, res) => {
     res.json = ({
