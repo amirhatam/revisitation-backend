04ae5203c945379a5c2eb67d00df2db7c1070ae3
Amir <amirhatam136@gmail.com>
2022-02-05 12:28:50 +0100

    finally add rooms on server

--- trippy_basics_api/server.js
+++ trippy_basics_api/server.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const mongoose = require('mongoose')
 const hotels = require('./routes/hotelsRoute')
 const restaurants = require('./routes/restaurantsRoute')
+const rooms = require('./routes/roomsRoute')
 
 mongoose.connect('mongodb://localhost:27017/trippy', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) {
@@ -21,6 +22,7 @@ app.use(express.json())
 
 app.use('/hotels', hotels)
 app.use('/restaurants', restaurants)
+app.use('/rooms', rooms)
 
 const port = 8000
 app.listen(port, () => {
