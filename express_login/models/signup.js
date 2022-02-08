9827789e743d5358253681e8ce469b1a23a0278e
Amir <amirhatam136@gmail.com>
2022-02-08 16:04:42 +0100

    create user model

---
+++ express_login/models/signup.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    confirmPass: { type: String, required: true },
+    firstName: { type: String, required: true },
+    surname: { type: String, required: true },
+    birthday: { type: Date, required: true },
+    created: { type: Date, default: Date.now }
+})
+User = mongoose.model.apply("User", userSchema)
+
+module.exports = User
---
+++ express_login/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require("./utils/config")
+
+
+mongoose.connect(config.mongoUrls, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+        console.log("There was a problem from connection to the database");
+    } else {
+        console.log("I'm connected to the database");
+    }
+})
+
+const port = config.port
+
+const app = express()
+
+app.listen(port, () => {
+    console.log('The server is waiting for requests');
+})
