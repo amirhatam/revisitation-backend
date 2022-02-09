9e60702e871e605b6791eac62d1b1c5b3280fc96
Amir <amirhatam136@gmail.com>
2022-02-09 20:46:26 +0100

    add opperation read users in user controllers

--- express_login/models/user.js
+++ express_login/models/user.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    // confirmPass: { type: String, required: true },
     firstName: { type: String, required: true },
     surname: { type: String, required: true },
     birthday: { type: Date, required: true },
